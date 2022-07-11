500cc46d9f9aef02237b545ef048872119386e18
SaLim Sayed <salemsayed981@gmail.com>
2022-07-11 23:50:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+import React from "react";
+
+import './App.css'
+import Forms from "./Forms";
+
+function App() {
+
+  return (
+    <div className="body">
+      <div className="container">
+
+        <Forms />
+      </div>
+    </div>
+  );
+}
+
+export default App;
